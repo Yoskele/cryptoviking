4f32a82080d7a904bec0364e0b79ef1e8351c8aa
yosef kayhko <yossi.kayhko@gmail.com>
2021-06-18 17:49:37 +0300

    f

--- website/website/settings.py
+++ website/website/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/yoseflotter/cryptoviking/website/assets'
+STATIC_ROOT = '/website/assets'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
