0da24ab05bbabba4cbdadafe60ec25305201ea8b
yosef kayhko <yossi.kayhko@gmail.com>
2021-06-18 18:02:35 +0300

    st

--- website/website/settings.py
+++ website/website/settings.py
@@ -108,13 +108,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
 )
+
 
 
 # Where to store the images from admin dashboard.
